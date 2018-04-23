b94517c4525da5a6a3da9053ba6cb3b4516a3f9c
angadkari <37918966+angadkari@users.noreply.github.com>
2018-04-23 16:27:26 -0500

    Delete csv.py

--- csv.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-with open("interface-desc.txt",'r') as raw_desc:
-	stripped  = (line.strip() for line in raw_desc)
-	csv_line = (line.split(",") for line in new_line if line)
-	with open('interface-desc.csv','w') as out_file:
-		writer = csv.writer(out_file)
-		writer.writerows(csv_line)
